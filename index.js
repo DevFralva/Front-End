9eed13689f2f50746209abf58c948598ddd40ae5
DevFralva <falvarador92@gmail.com>
2022-04-28 15:01:36 -0600

    Se añade calcular años,meses y días en milisegundos

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 import dayjs from "dayjs";
 
-imprimirConsola();
+let relativeTime = require("dayjs/plugin/relativeTime");
+dayjs.extend(relativeTime);
 
-function imprimirConsola() {
-    console.log(dayjs());
+const fecha1 = dayjs("1992-10-09");//fecha a comparar
+const fecha2 = dayjs();//fecha actual
+
+//console.log(dayjs().format("DD/MM/YYYY")); //muestra la fecha actual formateada
+
+let dias = fecha2.diff(fecha1, "day",true);
+let meses = fecha2.diff(fecha1, "month",true);
+let anios = fecha2.diff(fecha1, "year", true);
+
+let msMes = anios / meses;
+let msDia = anios / dias;
+//Los milisegundos de los años, meses y dias varia segun los años a comparar
+console.log(anios + " milisegundos es un año");
+console.log(msMes + " milisegundos es un mes");
+console.log(msDia + " milisegundos es un día");
+
+let contAnios = 0;
+let contMeses = 0;
+let contDias = 0;
+
+if (anios >= 1) {
+  while (anios >= 1) {
+    anios -= 1;
+    contAnios++;
+  }
 }
+
+while (anios >= msMes) {
+  anios -= msMes;
+  contMeses++;
+}
+
+while (anios >= msDia) {
+  anios -= msDia;
+  contDias++;
+}
+
+console.log(contAnios + " años");
+console.log(contMeses + " meses");
+console.log(contDias + " días");
