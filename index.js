b0dae47ca0cca839f6eb3ba22aea402b08b12202
DevFralva <falvarador92@gmail.com>
2022-05-02 16:23:11 -0600

    Actualiza calcular años,meses y días en MS

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 import dayjs from "dayjs";
 
 //Fechas a calcular
-const fecha1 = dayjs("1992-10-09");//fecha a comparar
-const fecha2 = dayjs();//fecha actual
+const fecha1 = dayjs("2021-01-01"); //fecha a comparar
+const fecha2 = dayjs("2021-12-31"); //fecha actual
 
 //console.log(dayjs().format("DD/MM/YYYY")); //muestra la fecha actual formateada
 
-let dias = fecha2.diff(fecha1, "day",true);
-let meses = fecha2.diff(fecha1, "month",true);
-let anios = fecha2.diff(fecha1, "year", true);
+let dias = Number.parseFloat(fecha2.diff(fecha1, "day", true));
+console.log(dias);
+let meses = Number.parseFloat(fecha2.diff(fecha1, "month", true));
+console.log(meses);
+let anios = Number.parseFloat(fecha2.diff(fecha1, "year", true));
+console.log(anios);
+console.log("___________________________________________")
 
-let msMes = anios / meses;
-let msDia = anios / dias;
+let totalMs = Number.parseFloat(fecha2.diff(fecha1, "milliseconds", true));
+
+let msAnio = Number.parseFloat(totalMs / anios);
+console.log(Number.parseFloat(totalMs / anios));
+
+let msMes = Number.parseFloat(totalMs / meses);
+console.log(Number.parseFloat(totalMs / meses));
+
+let msDia = Number.parseFloat(totalMs / dias);
+console.log(Number.parseFloat(totalMs / dias));
+console.log("___________________________________________")
+
 //Los milisegundos de los años, meses y dias varia segun los años a comparar
-console.log(anios + " milisegundos es un año");
+console.log(totalMs + " Milisegundos en total");
+console.log(msAnio + " milisegundos es un año");
 console.log(msMes + " milisegundos es un mes");
 console.log(msDia + " milisegundos es un día");
 
@@ -21,22 +36,22 @@ let contAnios = 0;
 let contMeses = 0;
 let contDias = 0;
 
-if (anios >= 1) {
-  while (anios >= 1) {
-    anios -= 1;
-    contAnios++;
-  }
+while (Number.parseFloat(totalMs) >= Number.parseFloat(msAnio)) {
+  totalMs -= Number.parseFloat(msAnio);
+  contAnios++;
 }
 
-while (anios >= msMes) {
-  anios -= msMes;
+while (Number.parseFloat(totalMs) >= Number.parseFloat(msMes)) {
+  totalMs -= Number.parseFloat(msMes);
   contMeses++;
 }
-
-while (anios >= msDia) {
-  anios -= msDia;
+console.log(totalMs);
+while (Number.parseFloat(totalMs) >= Number.parseFloat(msDia)) {
+  totalMs -= Number.parseFloat(msDia);
   contDias++;
 }
+
+console.log(dayjs(totalMs).diff(msDia, "hours"));
 
 console.log(contAnios + " años");
 console.log(contMeses + " meses");
